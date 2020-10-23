0ece9a600b2e8c775c30babb8e6ae422f2bceb74
unknown <technicalfriend.help@gmail.com>
2020-10-23 19:24:25 +0530

    blog website

---
+++ main.py
@@ -0,0 +1,11 @@
+from app.server import app
+import os
+
+production = os.environ.get("PRODUCTION", False)
+
+if __name__ == "__main__":
+    if production:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+        app.run(debug=True)
+    else:
+        app.run(debug=True,port="2020")
