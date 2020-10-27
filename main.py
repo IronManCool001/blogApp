7f51dc243cb8364a44416ac1223c6772dbf433a3
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2020-10-27 08:17:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from app.server import app
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 production = os.environ.get("PRODUCTION", False)
 
 if __name__ == "__main__":
     if production:
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
         app.run(debug=True)
     else:
         app.run(debug=True,port="2020")
